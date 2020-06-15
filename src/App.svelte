385582af0b9c44c08db7018c00af8baafdf31342
egotom <egotom@gmail.com>
2020-06-15 15:01:56 +0800

    Idiot update

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script>
 import { onMount } from 'svelte';
 import { writable } from 'svelte/store';
-import Solt from './component/solt.svelte';
-let s=[], luckMe=[], lst, me=[],topN=0,code=33333, optCode;
+import Ticket from './component/ticket.svelte';
+let s=[], luckMe=[], lst, me=[],code=33333;
 let  modal=false, confirm=false;
 let lb="B", error="", dell="";
 let soltMax=1;
@@ -20,7 +20,7 @@ function Init(){
 		for(let j=0;j<soltMax;j++){
 			col.push(0);
 		}
-		s.push(col);
+		s.push({"dpt":"","name":"","desc":"","ts":"","no":"","green":true});
 	}
 }
 
@@ -31,82 +31,45 @@ function Pause(){
 
 function Start(){
 	s=[];
-	ts=window.setTimeout(Start,20);
+	ts=window.setTimeout(Start,2);
 	for(let i=0 ; i<luckMax ;){
 		let n1="";
 		for(let j=0; j<soltMax; j++){
 			let number=Math.floor(Math.random()*10);
 			n1+=number.toString();
 		}
-		if(parseInt(n1)>topN)
+		if(luckMe.indexOf(n1)>-1)
 			continue;
-		s.push(n1);
-		i++;
-	}
-}
-function AddLuck(lkm){
-	let me=lb+'-';
-	for(let i in lkm){
-		me+=lkm[i];
-	}
-	luckMe=[me, ...luckMe];
-	LM.subscribe(localStorage.setItem("luckMe",JSON.stringify(luckMe)));
-}
-
-function DelLuck(lkm){
-	code=Math.floor(Math.random()*100000);
-	confirm=true;
-	error="";
-	optCode="";
-	dell=lkm;
-}
-function DelConfirm(){	
-	if(optCode==code){
-		confirm=false;
-		luckMe=luckMe.filter((me)=>me!=dell);
-		LM.subscribe(localStorage.setItem("luckMe",JSON.stringify(luckMe)));
-	}
-	code=Math.floor(Math.random()*100000);
-	error="操作确认码错误，请重新输入。";
-}
-
-function DelCancel(){
-	confirm=false;
-}
-function Bingo(v){
-	let me='';
-	for(let i in v){
-		me+=v[i];
-	}
-	let nb=parseInt(me);
-	let ok=false;
-	for(let i in lst){
-		if(nb==lst[i][0]){
-			ok=true;
-			break;
-		}
-	}
-	let ding=lb+'-'+me;
-	for(let j in luckMe){
-		if(luckMe[j]==ding)
-			return false;
-	}
-	return ok;
-}
-function doModal(v){
-	modal=!modal;
-	if(typeof v=="string"){
-		console.log(v.substring(2));
-		me=[];
-		let nb=parseInt(v.substring(2));
-		for(let i in lst){
-			if(nb==lst[i][0]){
-				me=lst[i];
+		let n2=null;
+		for(let d in lst){
+			if(lst[d][0]==n1){
+				n2={"dpt":lst[d][2] ,"name":lst[d][3] ,"desc":lst[d][4] ,"ts":lst[d][5] ,"no":lst[d][0],"green":false}
 				break;
 			}
 		}
+		if(n2==null)
+			continue;
+		s.push(n2);
+		i++;
 	}
 }
+
+function AddLuck(lkm){	
+	for(let i in s){
+		if(s[i].no==lkm.no)
+			s[i].green=true;
+	}
+	modal=!modal;
+	me=lkm;
+	luckMe=[lkm.no, ...luckMe];
+	LM.subscribe(localStorage.setItem("luckMe",JSON.stringify(luckMe)));
+}
+
+
+function doModal(v){
+	modal=!modal;
+}
+
 onMount(async () => {
 	const res = await fetch(`http://120.26.118.222:5000/rflyts/4`);
 	let lty = await res.json();
@@ -114,11 +77,7 @@ onMount(async () => {
 	lb=lty.cpt;
 	lst=lty.lst;
 	Init();
-	
-	lty.lst.map(l=>{
-		if(parseInt(l[0])>topN)
-			topN=parseInt(l[0]);
-	})
+	//console.log(lst);
 });
 
 </script>
@@ -128,96 +87,56 @@ onMount(async () => {
 		<div class="back">
 			<div class="modal" on:click={doModal}>
 				<div class="info">
-					<p class="who">{me[2]} : {me[3]}</p>
-					<p class="sel">{lb}-{me[0]}</p>
-					<p class="why">因：{me[4]}</p>
-					<p class="when">{me[5]}</p>
+					<p class="who">{me.dpt} - {me.name}</p>
+					<p class="sel">{lb}-{me.no}</p>
+					<p class="why">{me.desc}</p>
+					<p class="when">{me.ts}</p>
 				</div>
 			</div>
-			
-		</div>
-	</div>
-	{/if}
-	{#if confirm}
-	<div class="backdrop">
-		<div class="back">
-			<div class="confirm">
-				<h3>请输入操作确认码，完成操作。</h3>
-				<p><b class="code">{code}</b></p>
-				<p style="color:red">{error}</p>
-				<p>输入确认码 : <input type="text" bind:value={optCode} ></p>
-				<p><button on:click={DelConfirm}>确认</button><button on:click={DelCancel}>取消</button></p>	
-			</div>			
 		</div>
 	</div>
 	{/if}
 	<div class="dash">
 		<div class="opt">
-			<span class="label">获奖人数：</span><input type="text" on:keyup={Init} bind:value={luckMax} id="luckMax">
+			<span class="label">获奖人数：</span><input type="number" on:keyup={Init} bind:value={luckMax} id="luckMax" class="w70">
 			<br/>
 			<button on:click={Start}>开 始</button>
+			<br/>
 			<button on:click={Pause}>停 止</button>
-			<p style="color:red"><b>中奖序列号：</b></p>
-			<ul class="luckMe">
-				{#each luckMe as lm}
-					<li><b on:click={()=>doModal(lm)}>{lm}</b> <span class="btn" on:click={()=>DelLuck(lm)}><img alt="close" src="close.jpeg" /></span> </li>
-				{/each}
-			</ul>
 			<div class="open">
 				<a href="https://github.com/egotom/LuckyMe" target="_blank">开放源代码: github.com/egotom/LuckyMe</a>
 			</div>			
 		</div>
 		<div class="bod">
 			{#each s as lk}
-				{#if Bingo(lk)}
-					<Solt lk={lk} lb={lb} addOne={AddLuck} miss={false}/> 
-				{:else}
-					<Solt lk={lk} lb={lb} miss={true}/> 
-				{/if}
+				<div><Ticket tick={lk} addOne={AddLuck} cpt={lb}/> </div>
 			{/each}
 		</div>
-	</div>	
+	</div>
 </main>
 <style>
 	.dash{
 		display: grid;
-		grid-template-columns: 1fr 3fr;
+		grid-template-columns: 1fr 7fr;
 	}
-	.luckMe{
-		list-style: none;
-		border: 2px solid red;
-		margin-right:21px; 
-		margin-bottom: 20px;
-	}
-	.luckMe li b{
-		color: rgb(255,0,0);
-		font-size: 65px;
-		font-weight: bold;
-		text-decoration: none;
-	}
-	.luckMe li b:hover{
-		text-decoration: underline;
-		cursor:pointer;
-	}
+	.w70{max-width: 70px;}
 	.label{
 		font-size: 20px;
 		font-weight: bold;
 		color: rgb(0,0,255);
 	}
+	.bod{
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+
 	.open{
 		margin-top: 20px;
 		height:30px;
     	position: fixed;
     	bottom:10px;
 	}
-	.btn{line-height: 30px;}
-	.btn:hover{cursor:pointer;}
-	.btn img{
-		float: right;
-		width: 20px;
-		height: 20px;
-		margin: 37px 10px 15px 0;
-	}
+
 	.backdrop{
 		width: 100%;
 		height: 100%;
@@ -231,17 +150,7 @@ onMount(async () => {
 		margin:5% auto;
 		background-color: white;
 	}
-	.confirm{
-		padding: 30px;
-	}
-	.confirm p{
-		text-align: center;
-	}
-	.code{
-		color:red;
-		font-size: 40px;
-		line-height: 2em;
-	}
+
 	.modal{
 		border-radius: 5px;
 		width: 952px;
---
+++ src/component/ticket.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let tick={"dpt":'',"name":'',"no":'',"desc":'',"ts":'',"green":true}, addOne=null, cpt="";
+</script>
+<div class="tkt" class:gr={tick.green}  on:click={()=>addOne(tick)}>
+	<div class="hd">
+		<div class="to">{tick.dpt}-{tick.name}</div>
+		<div class="no">序列号：{cpt}-{tick.no}</div>
+	</div>
+	<div class="why">{tick.desc}</div>
+	<div class="hd">
+		<div></div>
+		<div class="ts">{tick.ts}</div>
+	</div>
+</div>
+
+
+<style>
+	.tkt{background-color:#cc0066;font-size: 20px;font-weight:bold;color:white;margin:10px;padding: 10px;}
+	.gr{background-color:#009933;font-size: 20px;font-weight:bold;color:white;margin:10px;padding: 10px;}
+	.tkt:hover{background-color: #cc0033;cursor:pointer;}
+	.gr:hover{background-color: #009900;cursor:pointer;}
+	.hd{display: grid;grid-template-columns: 5fr 2fr;}
+	.why{text-indent: 2em;line-height: 200%;}
+</style>
